86f577fa8ce55f6a9e40216be9d59cb29bc1e77a
Jessy <jessy.com>
2016-02-09 14:46:31 -0600

    my ascii art commit message

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(name = 'AsciiArt', 
+	version = '0.1.dev',
+	description='Ascii "art" from images',
+	author='clara',
+	packages=['asciiart'],
+	install_requires=['pillow'])
